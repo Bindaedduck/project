9f103735118bac347e55041ecb551554ce22ff28
Bindaedduck <seokjin.jang@kstec.co.kr>
2026-02-26 14:55:19 +0900

    db_to_processapp.py 만들기

---
+++ db_to_processapp.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+def execute():
+    engine = create_engine("postgresql://")
+    
+    df = pd.read_sql("SELCET * FROM jira_ticketing", con=engine)
+    
+    return df
+
+if __name__ = "__main__":
+    df = execute()
